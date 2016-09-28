816cab33a0e2ef46f41c9afb2965e4e62204315f
hrce3daniel <129473@hondsrugcollege.nl>
2016-09-28 15:30:47 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,9 @@ from User import User
 Jelte = User("Jelte", "1234", "129329@hondsrugcollege.nl")
 Matthijs = User("Matthijs", "4321", "129472@hondsrugcollege.nl")
 Jesper = User("Jesper", "0000", "129175@hondsrugcollege.nl")
+Daniël =User("Daniël", "9001", "129473@hondsrugcollege.nl")
 
 print(Jelte)
 print(Matthijs)
 print(Jesper)
+print(Daniël)
